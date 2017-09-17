12bc0e08ca1be95dd7f4f76e99f1019d4cd33108
vjviolet <gxzj_2015@163.com>
2017-09-17 19:56:59 +0800

    new

--- skin/js/change.js
+++ skin/js/change.js
@@ -1,16 +1,12 @@
-window.onload = function ()
-{
-	var oLink = document.getElementsByTagName("link")[0];
+window.onload = function() {
+	var oLink = document.getElementsByTagName('link')[0];
 	var oSkin = document.getElementById("skin").getElementsByTagName("li");
 	
-	for(var i = 0; i< oSkin.length; i++)
-	{		
-		oSkin[i].onclick = function ()
-		{
+	for(var i=0; i<oSkin.length; i++) {
+		oSkin[i].onclick = function() {
 			for(var p in oSkin) oSkin[p].className = "";
 			this.className = "current";
-			oLink['href'] = "css/"+this.id + ".css";				
-		}	
+			oLink['href'] = 'css/'+this.id+'.css';
+		}
 	}
-	
-};
+}
